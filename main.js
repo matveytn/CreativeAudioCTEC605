41636394542fddae20c31fcd3953a477bb7aca2d
Matt <latooma@gmail.com>
2016-10-25 15:03:33 +1300

    Code minification

--- main.js
+++ main.js
@@ -1,75 +1,32 @@
-var w = window.innerWidth, h = window.innerHeight;
+var w = window.innerWidth, h = window.innerHeight, r = (w<h) ? w / 3 : h / 3, x_off = 1000, y_off = 1000, z_off = 1000, vertices_amount = 180, px_offset = 120, NOISE_SCALE = 450, Z_SPEED = .01, X_SPEED = 0, Y_SPEED = 0, MOUSE_FORCE = -100, prevTime;
 function setup() {
 	createCanvas(w, h);
 	frameRate(60);
-	// filter(BLUR,30);
 }
-
-
-var r = (w<h) ? w / 3 : h / 3; // radius is a third of the smaller screen dimension
-var x_off = 1000, y_off = 1000, z_off = 1000;
-var vertices_amount = 180; //amount of vertices
-
-var px_offset = 120;    // amplitude
-var NOISE_SCALE = 450;  // the higher the softer
-
-
-var Z_SPEED = .01; // noise change per frame
-
-var X_SPEED = 0;
-var Y_SPEED = 0;
-
-
-var MOUSE_FORCE = -100;
-// positive 'push', negative 'pull'
-
-
-var dom_fps = document.getElementById("fps");
-var prevTime;
-
-// console.logMath.sqrt((Math.pow(centerCurX, 2) + Math.pow(centerCurY, 2)));
-
 function draw() {
-
-	// change noise direction with mouse
 	var mouseVector = createVector(mouseX / w - .5, mouseY / h - .5);
 	mouseVector.mult(MOUSE_FORCE);
 	X_SPEED = mouseVector.x;
 	Y_SPEED = mouseVector.y;
-
 	var rgb = sqrt(sq(mouseX - w / 2) + sq(-(mouseY - h / 2))) / 3.3;
-	if (rgb>=255)
-		rgb = 255;
-	else if (rgb<=72)
-		rgb = 72;
-	// console.log(rgb);
+	(rgb>=255) ? rgb = 255 : (rgb<=72) ? rgb = 72 : null;
 
-
-	// draw shape:
 	push();
 	translate(w / 2, h / 2);
 	background(219, 217, 201);
 	noStroke();
-	fill(rgb, 115, 133);    // color
+	fill(rgb, 115, 133);
 	beginShape();
 	drawingContext.shadowOffsetX = 0;
 	drawingContext.shadowOffsetY = 0;
 	drawingContext.shadowBlur = 1000;
 	drawingContext.shadowColor = "rgba(255 , 115, 133, 0.2)";
-	// console.log(drawingContext.shadowColor);
 	for (var a = 0; a<TWO_PI; a += TWO_PI / vertices_amount) {
-		// var x = r * sin(a)*mouseVector.y/-10;
-		// var y = r * cos(a)*mouseVector.x/10;
-		var x = r * sin(a);
-		var y = r * cos(a);
-
-		var new_x = x + (
+		var x = r * sin(a), y = r * cos(a), new_x = x + (
 			noise(
 				((x_off + x) / NOISE_SCALE),
 				((y_off + y) / NOISE_SCALE),
-				z_off) * px_offset * sin(a));
-
-		var new_y = y + (
+				z_off) * px_offset * sin(a)), new_y = y + (
 			noise(
 				((x_off + x) / NOISE_SCALE),
 				((y_off + y) / NOISE_SCALE),
@@ -78,9 +35,6 @@ function draw() {
 	}
 	endShape();
 	pop();
-
-
-	// update NOISE offsets
 	z_off += Z_SPEED;
 	x_off += X_SPEED;
 	y_off += Y_SPEED;
--- sound.js
+++ sound.js
@@ -1,78 +1,29 @@
-var audioContext = new AudioContext();
-// var source = audioContext.createMediaElementSource(myAudio);
-
-var anotherGainNode = audioContext.createGain();
-// var bass = 10;
-var bassFilter = audioContext.createBiquadFilter();
-bassFilter.type = "lowshelf";
-bassFilter.frequency.value = 200;  // switches to 400 in UI
-// bassFilter.gain.value = bass;  // you'll need to hook this to UI too
-
-// spec initial values
-var WIDTH = window.innerWidth;
-var HEIGHT = window.innerHeight;
-
-var canvas = document.querySelector('#fps');
-canvas.width = WIDTH;
-canvas.height = HEIGHT;
-
-var maxFreq = 2000;
-var maxVol = 1;
-
-var initFreq = 100;
-var initVol = 0.5;
-
-//Mouse pointer
-
-var CurX, CurY;
-
-document.onmousemove = updatePage;
-
-
-var canvasCtx = canvas.getContext('2d');
-
-
-var url = 'test.mp3';
-
-var source = audioContext.createBufferSource();
-source.connect(bassFilter);
-bassFilter.connect(anotherGainNode);
-anotherGainNode.connect(audioContext.destination);
-
-
-function updatePage(e) {
+var ac = new AudioContext(), gn = ac.createGain(), b = ac.createBiquadFilter(), vol = 1, src = ac.createBufferSource(), w = window.innerWidth, h = window.innerHeight, canvas = document.querySelector('#c');
+b.type = "lowshelf";
+b.frequency.value = 200;
+src.connect(b);
+b.connect(gn);
+gn.connect(ac.destination);
+canvas.width = w;
+canvas.height = h;
+document.onmousemove = function uP(e) {
 	CurX = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
 	CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
-
-	centerCurX = CurX - WIDTH / 2;
-	centerCurY = -(CurY - HEIGHT / 2);
-
-	anotherGainNode.gain.value = Math.sqrt(Math.pow(centerCurX, 2) + Math.pow(centerCurY, 2)) / HEIGHT * maxVol * 5;
-	source.playbackRate.value = Math.sqrt(Math.pow(centerCurX, 2) + Math.pow(centerCurY, 2)) / HEIGHT + 0.1;
-	bassFilter.gain.value = Math.sqrt(Math.pow(centerCurX, 2) + Math.pow(centerCurY, 2)) / HEIGHT * 4;
-
-	// console.log(bassFilter.gain.value);
+	c = Math.sqrt(Math.pow(CurX - w / 2, 2) + Math.pow(-(CurY - h / 2), 2));
+	gn.gain.value = c / h * vol * 5;
+	src.playbackRate.value = c / h + 0.1;
+	b.gain.value = c / h * 4;
 }
-
-
-/* --- load buffer ---  */
-var request = new XMLHttpRequest();
-//open the request
-request.open('GET', url, true);
-//webaudio paramaters
-request.responseType = 'arraybuffer';
-//Once the request has completed... do this
-request.onload = function () {
-	audioContext.decodeAudioData(request.response, function (response) {
-		/* --- play the sound AFTER the buffer loaded --- */
-		//set the buffer to the response we just received.
-		source.buffer = response;
-		//start(0) should play asap.
-		source.start(0);
-		source.loop = true;
+var rq = new XMLHttpRequest();
+rq.open('GET', 's.mp3', true);
+rq.responseType = 'arraybuffer';
+rq.onload = function () {
+	ac.decodeAudioData(rq.response, function (rsp) {
+		src.buffer = rsp;
+		src.start(0);
+		src.loop = true;
 	}, function () {
-		console.error('The request failed.');
+		console.error('fail');
 	});
 }
-//Now that the request has been defined, actually make the request. (send it)
-request.send();
+rq.send();
